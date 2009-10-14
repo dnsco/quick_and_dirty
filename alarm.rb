772a32ac430c9184d0453b642c67cc935b05c7f4
Dennis Collective <dennis.collective@gmail.com>
2009-10-14 01:51:01 -0700

    much prettier, now using modules and such

--- alarm.rb
+++ alarm.rb
@@ -1,71 +1,64 @@
 #!/usr/bin/env ruby
 
-# set $os with either mac or linux
-$os = "linux"
-
-class MixerMac
-	
-	def mute
-		system "osascript -e 'set Volume 0'" 
-	end
-
-	def mix
-		0.upto(10) { |i| system "osascript -e 'set Volume #{i}'"; sleep(15) }
-	end	
-
+module MixerLinux
+  def mute
+    system "amixer -c 0 -- sset Master 0%"
+  end
+  def mix _from=42, _to=85
+    _from.upto(_to) { |i| system "amixer -c 0 -- sset Master #{i}%"; sleep(1) }	
+  end
 end
 
-class MixerLinux
-
-	def mute
-		system "amixer -c 0 -- sset Master 0%"
-	end
-
-	def mix
-		42.upto(82) { |i| system "amixer -c 0 -- sset Master #{i}%"; sleep(1) }	
-	end
-
+module PlayerLinux
+  def play(song)
+    system "mpg123 '#{song}' &"
+  end
 end
 
-	
+class Alarm 
+  include MixerLinux
+  include PlayerLinux
 
+  ##include "Mixer#{self.os.upcase}".to_sym
+  ##include  "Player#{self.os.upcase}".to_sym
+  #include MixerLinux
+  #include  PlayerLinux
 
-class Playlist
+  def initialize
+    self.os
+    eval "self.mute"
+    eval "self.play(self.playlist)"
+    self.mix
+  end
 
   def playlist
-    @playlist = ARGV[0]
+    @playlist = ARGV[0] ? ARGV[0] : "playlist.m3u"
+  end
+
+  def os
+    @os = case RUBY_PLATFORM
+          when /linux/ then "Linux"
+          when /darwin/ then "Mac"
+          end
   end
 
 end
 
-
-class PlayerLinux
-
-  def play(song)
-		system "mpg123 '#{song}' &"
-	end
-
+Alarm.new
+module Alarm::MixerMac
+  def mute
+    system "osascript -e 'set Volume 0'" 
+  end
+  def mix
+    0.upto(10) { |i| system "osascript -e 'set Volume #{i}'"; sleep(15) }
+  end	
 end
 
-class PlayerMac
-  
+
+
+module Alarm::PlayerMac
   def play(song)
     system "/Applications/VLC.app/Contents/MacOS/VLC #{song} -Z &"
   end
-
 end
 
-case $os
-when "linux"
-  @mixer = MixerLinux.new
-  @player = PlayerLinux.new
-when "mac"
-  @mixer = MixerMac.new
-  @player = PlayerMac.new
-end
-		
-@playlist = Playlist.new
-
-@mixer.mute
-@player.play(@playlist)
-@mixer.mix
